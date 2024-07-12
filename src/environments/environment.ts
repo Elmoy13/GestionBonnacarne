496a75daf61adbebfa935355a88c1992f60c5bbe
DESKTOP-BL89A2L\chely <moyses123467890@gmail.com>
2024-07-11 23:05:27 -0600

    firebase bonnacarne

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,17 +1,17 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: true,
-  API: 'http://201.159.34.30:9295/bonnacarne-api/public/api/'
+  adminEmail: 'admin@bonnacarne.com',
+  API: 'http://201.159.34.30:9295/bonnacarne-api/public/api/',
+  superAdminEmail: 'sistemas@bonnacarne.com',
+  currency: '$',
+  firebase: {
+    apiKey: "AIzaSyCkitavzHWJZAaNlvaz9ACFzU7WmmWkPwY",
+    authDomain: "bonnacarne-88c7b.firebaseapp.com",
+    databaseURL: "https://bonnacarne-88c7b-default-rtdb.firebaseio.com",
+    projectId: "bonnacarne-88c7b",
+    storageBucket: "bonnacarne-88c7b.appspot.com",
+    messagingSenderId: "177178247256",
+    appId: "1:177178247256:web:25fce29f4a0df21cf14a2d",
+    measurementId: "G-VKXSG4SYTW"
+  }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
